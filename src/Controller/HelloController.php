1d9e65e5d5a46048aefebf32f519dae9fcf7fef1
Thomas Gravy <gravythomas@gmail.com>
2020-08-24 19:46:44 +0200

    HelloController, route, twig découverte

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController extends AbstractController {
+
+    function hello() {
+        //return  $this->render('base.html.twig');
+        // return $this->redirectToRoute('/');
+        throw $this->createNotFoundException();
+    }
+}
